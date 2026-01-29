bb0227bf32712f986854836a3fc53b3845429811
AkhilRaparthi <raparthiakhil55@gmail.com>
2026-01-29 20:15:14 +0530

    update02

--- Shell-practice/05-data-types.sh
+++ Shell-practice/05-data-types.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 NUM1=100
-NUM2=Sai
+NUM2=sivakumar
 
-SUM=$((NUM1+NUM2))
+SUM=$(($NUM1+$NUM2))
 
-echo sum is $SUM"
+echo "Sum is: $SUM"
 
-# Arrays
+# Array
+FRUITS=("Apple" "Banana" "Pomo")
 
-FRUITS=("Apple" "Orange" "Grapes")
-
-echo "First fruit is: ${FRUITS[0]}"
-echo "Second fruit: ${FRUITS[1]}"
-echo "All fruits: ${FRUITS[@]}"
+echo "Fruits are: ${FRUITS[@]}"
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
