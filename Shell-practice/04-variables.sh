4603d0a5c2b2575ce0424f897e8e8f044456793f
AkhilRaparthi <raparthiakhil55@gmail.com>
2026-01-29 20:03:54 +0530

    update02

--- Shell-practice/04-variables.sh
+++ Shell-practice/04-variables.sh
@@ -12,3 +12,5 @@ echo "PID of this script: $$"
 sleep 100 &
 echo "PID of recently executed background process: $!"
 echo "All args passed to script: $*"
+sleep 10 &
+echo "PID of recently executed background process: $!"
