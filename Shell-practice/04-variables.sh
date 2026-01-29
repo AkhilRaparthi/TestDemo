9df79394d1ad51d1cccbc9932b3dbd75fbadac57
AkhilRaparthi <raparthiakhil55@gmail.com>
2026-01-29 19:59:22 +0530

    update02

--- Shell-practice/04-variables.sh
+++ Shell-practice/04-variables.sh
@@ -11,5 +11,5 @@ echo "Home directory of current user: $HOME"
 echo "PID of this script: $$"
 sleep 100 &
 echo "PID of recently executed background process: $!"
-echo "PID of recently executed background process: $$"
+echo "ID of this script: $$"
 echo "All args passed to script: $*"
